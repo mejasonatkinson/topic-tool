c2756fbc04babab32986acfb2b254dccf9ce4e7c
mejasonatkinson <112265427+mejasonatkinson@users.noreply.github.com>
2022-09-21 21:06:48 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -75,27 +75,26 @@ let topic = Math.floor((Math.random() * topics.length) + 0);
 
 // Topics to add:
 
-//     "lodash",
-//     "axios",
+// lodash
+// axios
 
-//     "Processing",
-//     "Arduino",
-//     "p5JS",
+// Processing
+// Arduino
+// p5JS
 
-//     "AWS",
+// AWS
 
-//     "Adobe",
-//     "Photoshop",
+// Adobe
+// Photoshop
 
-//     "Unity",
-//     "Unreal Engine",
+// Unity
+// Unreal Engine
 
-//     "Autodesk",
-//     "Maya",
-//     "3ds Max",
+// Autodesk
+// Maya
+// 3ds Max
 
-//     "OBS",
-
+// OBS
 
 // Marketing
 
@@ -115,91 +114,7 @@ let topic = Math.floor((Math.random() * topics.length) + 0);
 // GitLab
 // BitBucket
 
-
 // postman
 // sanity.io
-
-
-// Project ideas:
-
-// https://www.youtube.com/watch?v=n2B-FClr5rA
-
-// 5 Coding Projects (from beginner to advanced)
-
-// beginner
-
-// 2:44 Beginner Project #1 - Hello, World (seriously)
-// 3:25 Beginner Project #2 - Cooking (data types & functions)
-// 7:03 Beginner Project #3 - Tournament Simulation (OOP, data sctructures, etc.)
-
-// intermediate
-
-// 9:16 Intermediate Project - Web App w/ APILayer
-
-// advanced
-
-// 15:25 Advanced Project - Content Management System
-
-// Hobby based
-// Tatical based
-
-
-
-
-// PHP Projects
-
-// https://www.youtube.com/playlist?list=PLS46nnSnLF7ob6bgt2Xtn1iqj8wBKpq-N
-// -> Chat System Using PHP
-// -> Invoicing System In PHP 
-// -> Social Networking Site In PHP
-// -> SIMPLE CHAT SYSTEM IN PHP 
-// -> Movie Ticketing System In PHP 
-// -> Job Portal Site Using PHP
-// -> SIMPLE CHAT SYSTEM IN PHP 
-// -> Messenger Chat System in PHP and MySQL 
-// -> Social Media Site in PHP
-
-// https://www.youtube.com/playlist?list=PLpwngcHZlPaf1aw42OGyitm4jh2Dlmi9c
-// ->
-
-// - Quiz
-// - Course
-// - Instrument Tuner
-// - CMS (Content Management Systems)
-// - To do List
-// - Invoice Generator
-// - Chess Game
-// - LMS (Learning Management System)
-// - Job Search Tool
-// - CV/Resume Generator
-// - Cover Letter Generator
-// - presentation tool
-// - portfolio website
-
-// Clones
-// - Slack, clone
-// - Twitch, clone
-// - Facebook, clone
-// - Reddit, clone
-// - Youtube, clone
-// - TikTok, clone
-// - Discord, clone
-// - Instagram, clone
-// - Trello, clone
-// - LinkedIn, clone
-// - Twitter, clone
-
-
-let projects = {
-    PHP: [
-        "",
-    ],
-    Docker: [
-        "",
-    ],
-    NodeJS: [
-        "",
-    ],
-}
 
 document.getElementById('topic').innerHTML = topics[topic];
